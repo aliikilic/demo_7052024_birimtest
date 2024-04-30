bdc23c9dfc6f5a571e906b1cb87f51c5d1b75706
aliikilic <alikilic0@yaani.com>
2024-04-30 21:40:14 +0300

    Junit test ornekleri eklendi

---
+++ BirimTestOrnek/src/main/java/tr/edu/medipol/java/BirimTestOrnek/Hesap.java
@@ -0,0 +1,18 @@
+package tr.edu.medipol.java.BirimTestOrnek;
+
+public class Hesap {
+
+	public static int Topla(int sayi1 , int sayi2) {
+		return sayi1 + sayi2;
+	}
+	
+	public static int Cikart(int sayi1, int sayi2) {
+		return sayi1 - sayi2;
+	}
+	public static int Carp(int sayi1, int sayi2) {
+		return sayi1 * sayi2;
+	}
+	public static int Bol(int sayi1, int sayi2) {
+		return sayi1 / sayi2;
+	}
+}
